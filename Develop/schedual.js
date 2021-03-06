b895c6beb49e554ea28bae27c965abfd66f0270c
claire ogesen <claire.ogesen.co@gmail.com>
2021-03-06 14:29:32 -0700

    paged laid out with bootstraps

---
+++ Develop/schedual.js
@@ -0,0 +1,41 @@
+//things that are going to get update 
+//document read W3 schools this - make sure dom is ready? 
+
+
+$( document ).ready(function() {
+
+
+
+var currentDay = document.getElementById("#currentDay")
+
+function updateTime() {
+    var today = moment();
+    var humanReadable = today.format('MMMM Do YYYY, h:mm:ss a');
+    currentDay.textContent = "humanReadable";
+    //$var(currentDay).append(currentDate);
+};
+
+setInterval(updateTime, 1000);
+
+updateTime();
+
+//$var("#currentDay").append(currentDate);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
