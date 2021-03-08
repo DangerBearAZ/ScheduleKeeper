1d7291ebf12e35fe88d4c8c73e45cddbe646db16
claire ogesen <claire.ogesen.co@gmail.com>
2021-03-07 17:45:13 -0700

    date works

--- Develop/schedual.js
+++ Develop/schedual.js
@@ -1,23 +1,12 @@
 //things that are going to get update 
 //document read W3 schools this - make sure dom is ready? 
-
-
-$( document ).ready(function() {
-
-var currentDay = document.getElementById("#currentDay")
-
-function updateTime() {
-    var today = moment();
-    var humanReadable = today.format('MMMM Do YYYY, h:mm:ss a');
-    currentDay.textContent = humanReadable;
-    $var(currentDay).append(humanReadable);
-};
-setInterval(updateTime, 1000);
-
-updateTime();
-
-//$var("#currentDay").append(currentDate);
-
-
+$(document).ready(function () {
+    
+    // var time = moment()
+    
+    // f {unction setPlanner()
+    
+    ///time set 
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 });
